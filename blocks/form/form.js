8d1c90ddcbc326ededb80445c9c497397358d5d2
lovelymandal16 <lovelymandal16@gmail.com>
2025-07-05 13:20:54 +0530

    bringing logic together

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -23,9 +23,83 @@ import {
   createInput,
 } from './util.js';
 
+// Import JSON assets
+const datefield = require("./asset/datefield.json");
+const defaultPageTemplate = require("./asset/defaultPageTemplate.json");
+
 export const DELAY_MS = 0;
 let captchaField;
 let afModule;
+
+// Transform JSON functions
+function transformJson(inputJson) {
+    const result = transformJsonUtil(inputJson);
+    const dor = getOrCreateDor(result);
+    dor.pageTemplate = defaultPageTemplate.pageTemplate;
+    console.log(result);
+    return result;
+}
+
+const transformJsonUtil = (inputJson) => {
+    const result = {};
+    for (const key in inputJson) {
+        if (inputJson.hasOwnProperty(key)) {
+            const value = inputJson[key];
+            if (value !== null && Array.isArray(value) && key === "items") {
+                transformArray(result, value);
+            } else {
+                result[key] = value;
+            }
+        }
+    }   
+    return result;
+}
+
+const transformArray = (result, array) => {
+    if (array.length === 0) {
+        return;
+    }
+    result[":items"] = {};
+    result[":itemsOrder"] = [];
+    for (const item of array) {
+        result[":items"][item.id] = transformJsonUtil(item);
+        addDorContainer(result[":items"][item.id]);
+        result[":itemsOrder"].push(item.id);
+    }
+}
+
+const addDorContainer = (item) => {
+    const fieldTypeJson = fieldTypeBasedJson(formdefTypeToCRISPRType[item.fieldType]);
+    const dor = getOrCreateDor(item);
+    dor.dorContainer = fieldTypeJson;
+    const dataRef = item["dataRef"];
+    if(dataRef){
+        const bind = {
+            "ref": dataRef,
+            "match": "dataRef"
+        }
+        item["properties"]["fd:dor"]["dorContainer"]["bind"] = bind;
+    }
+}
+
+const getOrCreateDor = (item) => {
+    item["properties"] = item["properties"] || {};
+    item["properties"]["fd:dor"] = item["properties"]["fd:dor"] || {};
+    return item["properties"]["fd:dor"];
+}
+
+const formdefTypeToCRISPRType = {
+    "date": "datetimefield"
+};
+
+const fieldTypeBasedJson = (type) => {
+    switch(type){
+        case "datetimefield":
+           return datefield;
+        default:
+            return null;
+    }
+};
 
 const withFieldWrapper = (element) => (fd) => {
   const wrapper = createFieldWrapper(fd);
@@ -856,12 +930,14 @@ export default async function decorate(block) {
      // createMasterXDP();
       if (isDocumentBasedForm(formDef)) {
         const transform = new DocBasedFormToAF();
-    
-var headers= new Headers(); 
-    headers.append('Authorization', 'Basic ' + btoa('admin:admin'));
-    headers.append('Content-Language', 'en-US');
-    headers.append('Content-Type', 'application/json;charset=utf-8');
-    headers.append('x-adobe-accept-unsupported-api', '1');
+        formDef = transform.transform(formDef);
+        formDef = transformJson(formDef);
+        console.log(formDef);
+        var headers = new Headers(); 
+        headers.append('Authorization', 'Basic ' + btoa('admin:admin'));
+        headers.append('Content-Language', 'en-US');
+        headers.append('Content-Type', 'application/json;charset=utf-8');
+        headers.append('x-adobe-accept-unsupported-api', '1');
         var data = JSON.stringify(formDef);
         // Fetch the response
         var options = {
